a51e41af13302035210e89b9e4f4fcfaf3a44f3a
Pratik5862 <pratikmahajan768@gmail.com>
2021-10-13 18:57:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+    return (
+        <div>
+            <h1>Hello from React JS</h1>
+            <br />
+            <p>Hello from Java</p>
+             <br />
+            <h2>Hello from Node.JS</h2>
+        </div>
+    );
+
+}
+
+export default App;  
